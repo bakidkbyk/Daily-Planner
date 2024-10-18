6dbd5b6c74f2a86d775a7ee33193bbe333737f1b
bakidkbyk <baki.ordulu@gmail.com>
2024-10-18 20:25:55 +0300

    add base

---
+++ Daily-Planner/Scenes/DetailPlanner/DetailPlannerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailPlannerViewController.swift
+//  Daily-Planner
+//
+//  Created by Baki Dikbıyık on 18.10.2024.
+//
+
+import UIKit
+
+final class DetailPlannerViewController: BaseViewController<DetailPlannerViewModel> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
